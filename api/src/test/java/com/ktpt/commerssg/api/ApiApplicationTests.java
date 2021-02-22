e7cc2fe162821b6f027df5751293c38b36cbbe38
Gyeongjun Kim <lastpuzly@gmail.com>
2021-02-22 21:50:26 +0900

    feat: api 모듈 구현

---
+++ api/src/test/java/com/ktpt/commerssg/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktpt.commerssg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- jpa/src/main/java/com/ktpt/commerssg/jpa/CommerceItemEntity.java
+++ jpa/src/main/java/com/ktpt/commerssg/jpa/CommerceItemEntity.java
@@ -1,6 +1,7 @@
 package com.ktpt.commerssg.jpa;
 
 import com.ktpt.commerssg.common.domain.model.CommerceItem;
+import com.ktpt.commerssg.common.domain.model.Platform;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,10 +17,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class CommerceItemEntity extends CommerceItem {
+public class CommerceItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
+    private Platform platform;
+    private String thumbnail;
+    private Integer originalPrice;
+    private Integer sellingPrice;
+    private Double discountRate;
+
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+
+    public static CommerceItemEntity toEntity(CommerceItem commerceItem) {
+        return new CommerceItemEntity(null, commerceItem.getTitle(), commerceItem.getPlatform(), commerceItem.getThumbnail(), commerceItem.getOriginalPrice(), commerceItem.getSellingPrice(), commerceItem.getDiscountRate(), null, null);
+    }
 }
