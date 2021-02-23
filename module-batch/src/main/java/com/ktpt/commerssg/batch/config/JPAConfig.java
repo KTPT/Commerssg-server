a607d0b2df3e8f7ef873fd37335b11f6a7478ab0
Gyeongjun Kim <lastpuzly@gmail.com>
2021-02-23 14:26:49 +0900

    fix: batch 모듈 오류 해결, ComponentScan 제거, 모듈 이름 변경 (prefix)

--- module-batch/src/main/java/com/ktpt/commerssg/batch/config/JPAConfig.java
+++ module-batch/src/main/java/com/ktpt/commerssg/batch/config/JPAConfig.java
@@ -1,13 +1,11 @@
 package com.ktpt.commerssg.batch.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.ktpt.commerssg.*")
-@EntityScan(basePackages = "com.ktpt.commerssg.*")
-@ComponentScan(basePackages = "com.ktpt.commerssg.*")
+@EnableJpaRepositories(basePackages = "com.ktpt.commerssg.jpa")
+@EntityScan(basePackages = "com.ktpt.commerssg.jpa")
 @Configuration
-public class ScanConfig {
+public class JPAConfig {
 }
